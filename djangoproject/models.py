6071473fa548578bc1d943497b924f9b9056e635
Jorge Aurelio Morales Manrique <jomorales@unal.edu.co>
2020-08-12 20:45:59 -0500

    commit

--- djangoproject/models.py
+++ djangoproject/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # TODO: crear los modelos para la base de datos
+
+class User(models.Model):
+    username = models.CharField(maxlength=20)
+    password = models.CharField(maxlength=20)
