ed695cbc64638e9f2fb7129d48ed86b21c7dbe53
Jorge Aurelio Morales Manrique <jomorales@unal.edu.co>
2020-08-13 10:01:46 -0500

    El archivo models.py fue movido al directorio app/

--- djangoproject/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# TODO: crear los modelos para la base de datos
-
-class User(models.Model):
-    username = models.CharField(maxlength=20)
-    password = models.CharField(maxlength=20)
