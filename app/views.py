b88a7967b4ea4617b6a2de1f86b0087aec64e6d4
Jorge Aurelio Morales Manrique <jomorales@unal.edu.co>
2020-08-13 16:57:19 -0500

    Página para subir archivos añadida.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+from app.models import File
 
 def login_view(request):
     return render(request, 'login.html', {})
 
 def desktop_view(request):
     return render_to_response(request, 'desktop.html', {})
+
+def upload(request):
+    if request.method == 'POST':
+        upload_file = request.FILES['document']
+        fs = FileSystemStorage()
+        name = upload_file.name
+        path = fs.save(upload_file.name, upload_file)
+        path = fs.url(path)
+        file = File(name=name, path=path)
+        file.save()
+    return render(request, 'upload.html', {})
