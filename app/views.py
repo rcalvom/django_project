d162d7f6ac7d61494c53e6843ed0626edc7bd9af
Ricardo Andrés Calvo <rcalvom@unal.edu.co>
2020-08-13 20:52:53 -0500

    Primer prototipo de inicio de sesión. Consulta correcta en la base de datos.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from app.models import User
 
 def login_view(request):
-    return render(request, 'login.html', {})
+    if request.GET:
+        username = request.GET["user"]
+        password = request.GET["password"]
+        user = User.objects.get(username=username)
+        if user:
+            if password  == user.password:
+                request.GET = None
+                return render(request, 'desktop.html', {})
+            else:
+                return render(request, 'login.html', {})
+    else:
+        return render(request, 'login.html', {})
+
 
 def desktop_view(request):
     return render(request, 'desktop.html', {})
